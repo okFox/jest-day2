9acd0a709391b43a5f8f69a5a002228a37f21567
okFox <smayfield@gmail.com>
2019-12-04 15:29:34 -0800

    stringify w/o filewrite passing

--- __tests__/fs.test.js
+++ __tests__/fs.test.js
@@ -6,6 +6,6 @@ const {
 
 describe('file system functions', () => {
   it('properly stringifys to JSON', () => {
-    expect(writeJSON({ name: 'chet', occupation: 'being annoying' })).toBe('{"name": "chet","occupation": "being annoying"}');
+    expect(writeJSON({ name: 'chet', occupation: 'being annoying' })).toEqual('{"name":"chet","occupation":"being annoying"}');
   });
 });
