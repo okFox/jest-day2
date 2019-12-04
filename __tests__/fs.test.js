4166811da523e0426a2a3d3950ffe1014c135d23
okFox <smayfield@gmail.com>
2019-12-04 15:48:53 -0800

    ready to add a mock for writefile

--- __tests__/fs.test.js
+++ __tests__/fs.test.js
@@ -1,7 +1,9 @@
 const {
-  mkdirp,
-  writeJSON
+  writeJSON,
+  writeJSONFile
 } = require('../lib/fs');
+
+const fs = require('fs').promises;
 
 
 describe('file system functions', () => {
@@ -9,3 +11,11 @@ describe('file system functions', () => {
     expect(writeJSON({ name: 'chet', occupation: 'being annoying' })).toEqual('{"name":"chet","occupation":"being annoying"}');
   });
 });
+
+//needs a MOCK to work
+// describe('writeJSONFile', () => {
+//   it('writes a stringified object to a file', () => {
+//     return writeJSONFile('path', {})
+//       .then(() => expect(fs.writeFile).toHaveBeenCalledWith('path', '{}'));
+//   });
+// });
